4157b7609889d0e309a812e84643bed2e421abdb
Rodrigo Cabral <rodrigoalcab@gmail.com>
2022-09-08 22:50:31 +0100

    passaport

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace App\Providers;
 
+
+
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+use Laravel\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,5 +29,6 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
         //
+        Passport::routes();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 */
 
-Route::resource('/fichas', \App\Http\Controllers\ApiFichaController::class);
+Route::middleware('auth:api')->Route::resource('/fichas', \App\Http\Controllers\ApiFichaController::class);
