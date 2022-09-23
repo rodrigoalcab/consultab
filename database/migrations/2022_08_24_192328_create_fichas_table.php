e993b5beb49d3491b4590c3ec0f38897a21ec85f
Rodrigo Cabral <rodrigoalcab@gmail.com>
2022-09-23 21:18:23 +0100

    update migrations

--- database/migrations/2022_08_24_192328_create_fichas_table.php
+++ database/migrations/2022_08_24_192328_create_fichas_table.php
@@ -15,12 +15,12 @@ return new class extends Migration
     {
         Schema::create('fichas', function (Blueprint $table) {
             $table->id();
-            $table->string('campo');
-            $table->string('faixas');
-            $table->string('codigo');
-            $table->string('objetivos');
-            $table->string('abordagem');
-            $table->string('sugestoes');
+            $table->foreign('campo_id')->after('id')->constrained('campos')->cascadeOnDelete();
+            $table->foreign('faixa_id')->after('id')->constrained('faixas')->cascadeOnDelete();
+            $table->foreign('codigo_id')->after('id')->constrained('codigos')->cascadeOnDelete();
+            $table->foreign('objetivo_id')->after('id')->constrained('objetivos')->cascadeOnDelete();
+            $table->foreign('abordagem_id')->after('id')->constrained('abordagens')->cascadeOnDelete();
+            $table->foreign('sugestao_id')->after('id')->constrained('sugestoes')->cascadeOnDelete();
             $table->timestamps();
         });
     }
